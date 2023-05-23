374f38e704504b2f9df1579227fed0a2c02c2f83
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-05-23 17:04:52 +1000

    Installed Quantum console

--- Assets/Game/Scripts/UI/MainMenu/MainMenuController.cs
+++ Assets/Game/Scripts/UI/MainMenu/MainMenuController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using QFSW.QC;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -11,11 +12,16 @@ public class MainMenuController : MonoBehaviour
     /// <summary>
     /// Starts the game
     /// </summary>
+    [Command]
     public void StartGame()
     {
         SceneManager.LoadScene(_gameScene, LoadSceneMode.Single);
     }
 
+    /// <summary>
+    /// Exits the game
+    /// </summary>
+    [Command]
     public void ExitGame()
     {
         #if UNITY_EDITOR
