2c23af8e05561b1857cdc41c35ea3cbf0a4379cd
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-05-28 21:51:59 +1000

    Added start command for endless

--- Assets/Game/Scripts/UI/MainMenu/MainMenuController.cs
+++ Assets/Game/Scripts/UI/MainMenu/MainMenuController.cs
@@ -11,17 +11,26 @@ public class MainMenuController : MonoBehaviour
     /// <summary>
     /// Starts the game
     /// </summary>
-    [Command]
+    [Command("Start")]
     public void StartGame()
     {
         SceneManager.LoadScene(_sceneData.Game, LoadSceneMode.Single);
     }
 
     /// <summary>
+    /// Starts the game in endless mode
+    /// </summary>
+    [Command("Start-Endless")]
+    public void StartEndless()
+    {
+        SceneManager.LoadScene(_sceneData.Endless, LoadSceneMode.Single);
+    }
+
+    /// <summary>
     /// Exits the game
     /// </summary>
-    [Command]
-    public void ExitGame()
+    [Command("Quit")]
+    public void QuitGame()
     {
         #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
