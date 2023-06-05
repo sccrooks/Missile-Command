5ab98a85a65fc44d15816954a2f22a507e1f7efe
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-05 22:56:20 +1000

    Reticle now moves, calling GameController.MoveReticle from within PlayerController

--- Assets/Game/Scripts/Controls/PlayerController.cs
+++ Assets/Game/Scripts/Controls/PlayerController.cs
@@ -1,4 +1,5 @@
 using MissileCommand.Controls;
+using MissileCommand.Gameplay.GameController;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -29,8 +30,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        Vector2 move = _controls.Reticle.Move.ReadValue<Vector2>();
-        Debug.Log(move);
+        GameController.Instance.MoveReticle(_controls.Reticle.Move.ReadValue<Vector2>());
 
         if (_controls.Reticle.Fire.triggered)
             Debug.Log("Fired missile");
