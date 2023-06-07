107d4d2a8be4bb7d8007cf8bb3d2057cae849a70
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-08 09:15:10 +1000

    Added ability to Un/register GameEventListeners within GameEvent.cs

--- Assets/Game/Scripts/Infrastructure/ScriptableObjects/GameEvent.cs
+++ Assets/Game/Scripts/Infrastructure/ScriptableObjects/GameEvent.cs
@@ -6,6 +6,21 @@ namespace MissileCommand.Infrastructure.ScriptableObjects
 {
     public class GameEvent : ScriptableObject
     {
-        
+        private List<GameEventListener> listeners = new List<GameEventListener>();
+
+        public void Raise()
+        {
+
+        }
+
+        public void RegisterListener(GameEventListener listener)
+        {
+            listeners.Add(listener);
+        }
+
+        public void UnregisterListener(GameEventListener listener)
+        {
+            listeners.Remove(listener);
+        }
     }
 }
