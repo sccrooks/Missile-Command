0e9630ca9cc291de1a5446362a533bf7707a68a3
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-08 09:11:10 +1000

    Created GameEvent scriptable object

---
+++ Assets/Game/Scripts/Infrastructure/ScriptableObjects/GameEvent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MissileCommand.Infrastructure.ScriptableObjects
+{
+    public class GameEvent : ScriptableObject
+    {
+        
+    }
+}
