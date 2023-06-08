65bcecc5c578ab23344f271b85007006afccf9be
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-08 19:03:00 +1000

    Added score to userdata object

--- Assets/Game/Scripts/Infrastructure/ScriptableObjects/UserData.cs
+++ Assets/Game/Scripts/Infrastructure/ScriptableObjects/UserData.cs
@@ -8,17 +8,18 @@ namespace MissileCommand.Infrastructure.ScriptableObjects
     public class UserData : ScriptableObject, ISerializationCallbackReceiver
     {
         public string Name;
+        public IntVariable Score;
 
         public void OnAfterDeserialize()
         {
             // TODO: Save name to file
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException();
         }
 
         public void OnBeforeSerialize()
         {
             // TODO: Load name from file
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException();
         }
     }
 }
