8225eec695cfa64e8dcd5444ecb47ccefecaf224
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-09 05:55:13 +1000

    Missile.cs MoveTowardsTarget method

--- Assets/Game/Scripts/Gameplay/Enemies/Missile.cs
+++ Assets/Game/Scripts/Gameplay/Enemies/Missile.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+
+    public void MoveTowardsTarget(Vector2 target)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, _speed * Time.deltaTime);
+    }
+
+
     public void BaseCollision()
     {
         Debug.Log("Collided with Base");
