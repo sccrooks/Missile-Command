50295d41297164b4e28742b431928996a128c8b2
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-09 05:59:27 +1000

    Created BasicBrain.cs

---
+++ Assets/Game/Scripts/Gameplay/Enemies/Brains/BasicBrain.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MissileCommand.Gameplay.Enemies
+{
+    [CreateAssetMenu(fileName = "Basic Brain", menuName = "Brains/Basic")]
+    public class BasicBrain : Brain
+    {
+        public override void Think()
+        {
+            
+        }
+    }
+}
