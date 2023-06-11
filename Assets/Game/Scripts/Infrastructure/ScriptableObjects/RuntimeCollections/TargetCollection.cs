d59138edd97e2cd7e5ec01324dc2a69c5dfabe34
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-12 00:03:24 +1000

    Created targetCollection.cs

---
+++ Assets/Game/Scripts/Infrastructure/ScriptableObjects/RuntimeCollections/TargetCollection.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MissileCommand.Infrastructure.ScriptableObjects
+{
+
+    public class TargetCollection : RuntimeCollection<GameObject>
+    {
+        
+    }
+}
