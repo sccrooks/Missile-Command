c630ad82e821ee89e6d07b291ed2127cb3de3dda
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-11 20:54:03 +1000

    Update BaseContainer.cs

--- Assets/Game/Scripts/Gameplay/Bases/BaseContainer.cs
+++ Assets/Game/Scripts/Gameplay/Bases/BaseContainer.cs
@@ -1,8 +1,14 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseContainer : MonoBehaviour
+namespace MissileCommand.Gameplay.Bases
 {
-    
+    [CreateAssetMenu(fileName = "Base Container", menuName = "Gameplay/Base Container")]
+    public class BaseContainer : ScriptableObject
+    {
+        public List<Base> ActiveBases = new List<Base>();
+        public List<Base> DestroyedBases = new List<Base>();
+
+
+    }
 }
