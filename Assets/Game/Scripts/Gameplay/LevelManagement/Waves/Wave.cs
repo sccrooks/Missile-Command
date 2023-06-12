174199a2f20ee7412375aa8245a60a1fd830a15d
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-12 21:59:21 +1000

    Update Wave.cs

--- Assets/Game/Scripts/Gameplay/LevelManagement/Waves/Wave.cs
+++ Assets/Game/Scripts/Gameplay/LevelManagement/Waves/Wave.cs
@@ -1,7 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Wave : ScriptableObject
+namespace MissileCommand.Gameplay.LevelManagement
 {
+    public abstract class Wave : ScriptableObject
+    {
+        public List<GameObject> missiles = new List<GameObject>();
 
-}
+        public abstract void SpawnMissiles();
+    }
+}  
