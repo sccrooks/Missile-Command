e75ab9f0a71981f1d07382ebcde9243ff4075751
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-12 22:41:46 +1000

    Basic wave type created

---
+++ Assets/Game/Scripts/Gameplay/LevelManagement/Waves/BasicWave.cs
@@ -0,0 +1,46 @@
+using MissileCommand.Gameplay.LevelManagement;
+using MissileCommand.Infrastructure.ScriptableObjects.Events;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MissileCommand.Gameplay.LevelManagement
+{
+    public class BasicWave : Wave
+    {
+        [SerializeField] private int _spawnDelay = 0;
+        [SerializeField] private int _spawnedEntites = 0;
+
+        [Header("Events")]
+        [SerializeField] private GameEvent _waveStarted;
+        [SerializeField] private GameEvent _waveEnded;
+        [SerializeField] private GameEvent _entitySpawnRequested;
+
+        public override void StartWave(GameEvent waveStarted, GameEvent waveEnded, GameEvent entitySpawnRequested)
+        {
+            // Get events
+            _waveStarted = waveStarted;
+            _waveEnded = waveEnded;
+            _entitySpawnRequested = entitySpawnRequested;
+
+            _spawnedEntites = 0;
+            _waveStarted?.Raise();
+            SpawnEntity();
+        }
+
+        public override void EndWave()
+        {
+            _waveEnded?.Raise();
+        }
+
+        public override async void SpawnEntity()
+        {
+            if (_spawnedEntites <= Entities.Count) return;
+            _spawnedEntites++;
+
+            await Task.Delay(_spawnDelay);
+            _entitySpawnRequested?.Raise();
+        }
+    }
+}
--- Assets/Game/Scripts/Gameplay/LevelManagement/Waves/Wave.cs
+++ Assets/Game/Scripts/Gameplay/LevelManagement/Waves/Wave.cs
@@ -1,12 +1,17 @@
+using MissileCommand.Infrastructure.ScriptableObjects.Events;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace MissileCommand.Gameplay.LevelManagement
 {
     public abstract class Wave : ScriptableObject
     {
-        public List<GameObject> missiles = new List<GameObject>();
+        [SerializeField]
+        protected List<GameObject> Entities = new List<GameObject>();
 
-        public abstract void SpawnMissiles();
+        public abstract void StartWave(GameEvent waveStarted, GameEvent waveEnded, GameEvent entitySpawnRequested);
+        public abstract void EndWave();
+        public abstract void SpawnEntity();
     }
 }  
