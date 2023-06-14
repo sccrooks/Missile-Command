79bf652b9b2c6efa170269c9d2e912e08c94b242
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-15 01:13:01 +1000

    Missile now calls MissileDestroyedEvent on destruction

--- Assets/Game/Scripts/Gameplay/Missile/Missile.cs
+++ Assets/Game/Scripts/Gameplay/Missile/Missile.cs
@@ -1,13 +1,19 @@
 using MissileCommand.Gameplay.Enemies;
 using MissileCommand.Infrastructure.ScriptableObjects;
+using MissileCommand.Infrastructure.ScriptableObjects.Events;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+    [SerializeField] private float _reward;
     [SerializeField] private float _speed;
     [SerializeField] private GameObjectCollection _activeEntities;
+
+    [Header("Events")]
+    [SerializeField] private FloatGameEvent _missileDestroyed;
 
     public AIThinker AIThinker;
 
@@ -24,6 +30,7 @@ public class Missile : MonoBehaviour
     public void Destroy()
     {
         _activeEntities.Remove(this.gameObject);
+        _missileDestroyed.Raise(_reward);
         Destroy(gameObject);
     }
 
