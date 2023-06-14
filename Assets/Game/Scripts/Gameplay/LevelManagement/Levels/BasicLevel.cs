88cb51e121bbb6067d1ff3ca8a5429f49cf0416f
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-14 15:32:46 +1000

    Created BasicLevel.cs

---
+++ Assets/Game/Scripts/Gameplay/LevelManagement/Levels/BasicLevel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MissileCommand.Gameplay.LevelManagement
+{
+    public class BasicLevel : Level
+    {
+        public override void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void End()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
