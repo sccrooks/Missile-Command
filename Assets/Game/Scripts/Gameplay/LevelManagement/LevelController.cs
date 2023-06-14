70a2c69ed9de6fe9fc7b702b5ae1f9b1d18f6153
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-14 14:55:33 +1000

    Update LevelController.cs

--- Assets/Game/Scripts/Gameplay/LevelManagement/LevelController.cs
+++ Assets/Game/Scripts/Gameplay/LevelManagement/LevelController.cs
@@ -1,3 +1,4 @@
+using MissileCommand.Infrastructure.ScriptableObjects;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,10 @@ namespace MissileCommand.Gameplay.LevelManagement
 {
     public abstract class LevelController : MonoBehaviour
     {
+        [Header("Components")]
+        [SerializeField] private GameObjectCollection _entitySpawnList;
+
         public abstract void Start();
+        public abstract void Update();
     }
 }
