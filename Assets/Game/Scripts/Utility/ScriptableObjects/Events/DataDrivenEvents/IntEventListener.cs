66459cada75cbf9a657592471f9ce622e7e552db
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-17 05:01:00 +1000

    Created IntEventListener

--- Assets/Game/Scripts/Utility/ScriptableObjects/Events/DataDrivenEvents/IntEventListener.cs
+++ Assets/Game/Scripts/Utility/ScriptableObjects/Events/DataDrivenEvents/IntEventListener.cs
@@ -2,20 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Sccrooks.Utility
+namespace Sccrooks.Utility.ScriptableObjects.Events
 {
     public class IntEventListener : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [Tooltip("Event to register with.")]
+        public IntEvent Event;
+
+        [Tooltip("Response to invoke when Event is raised.")]
+        public MyIntEvent Response;
+
+        private void OnEnable()
         {
-        
+            Event.RegisterListener(this);
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnDisable()
         {
-        
+            Event.UnregisterListener(this);
+        }
+
+        public void OnEventRaised(int data)
+        {
+            Response.Invoke(data);
         }
     }
 }
