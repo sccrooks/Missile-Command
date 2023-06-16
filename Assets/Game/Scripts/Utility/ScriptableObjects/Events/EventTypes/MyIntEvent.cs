7f9bc406f2ca7eb221939b73bda6aff5bcc3cd64
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-17 04:58:20 +1000

    Created MyIntEvent

---
+++ Assets/Game/Scripts/Utility/ScriptableObjects/Events/EventTypes/MyIntEvent.cs
@@ -0,0 +1,10 @@
+using UnityEngine.Events;
+
+namespace Sccrooks.Utility.ScriptableObjects.Events
+{
+    [System.Serializable]
+    public class MyIntEvent : UnityEvent<int>
+    {
+        
+    }
+}
