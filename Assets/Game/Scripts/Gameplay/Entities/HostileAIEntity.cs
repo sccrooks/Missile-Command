d1384e88c7ad4e2b660f58af2e61c2e98963d4e9
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-17 20:56:07 +1000

    Created HostileAIEntity.cs

---
+++ Assets/Game/Scripts/Gameplay/Entities/HostileAIEntity.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MissileCommand.Gameplay
+{
+    public class HostileAIEntity : AIEntity
+    {
+        
+    }
+}
