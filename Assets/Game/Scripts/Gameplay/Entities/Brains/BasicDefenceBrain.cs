4131898e49d27a46c56a56ca949c9a079ea85a7d
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-17 23:27:55 +1000

    Defense missiles fly to specific position

--- Assets/Game/Scripts/Gameplay/Entities/Brains/BasicDefenceBrain.cs
+++ Assets/Game/Scripts/Gameplay/Entities/Brains/BasicDefenceBrain.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 
 namespace MissileCommand.Gameplay.Entities
 {
+    [CreateAssetMenu(fileName = "Basic Brain", menuName = "Brains/Defense Basic Brain")]
     public class BasicDefenceBrain : Brain
     {
-        [SerializeField] private Vector2 _target;
         [SerializeField] private GameObjectVariable _reticle;
+        private Vector2 _target;
+        private bool _foundTarget = false;
 
         public override void FindTarget()
         {
             Vector3 reticlePosition = _reticle.RunTimeValue.gameObject.transform.position;
             _target = new Vector2(reticlePosition.x, reticlePosition.y);
+            _foundTarget = true;
         }
 
         public override void Think(AILogicController thinker)
         {
-            if (_target == null)
+            if (!_foundTarget)
                 FindTarget();
 
             thinker.MoveTowardsTarget(_target);
