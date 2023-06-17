2a0d9a035b819e6375242a46b66f67a7bb6507b5
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-17 23:14:18 +1000

    Update BasicDefenceBrain.cs

--- Assets/Game/Scripts/Gameplay/Entities/Brains/BasicDefenceBrain.cs
+++ Assets/Game/Scripts/Gameplay/Entities/Brains/BasicDefenceBrain.cs
@@ -1,3 +1,4 @@
+using Sccrooks.Utility.ScriptableObjects.Variables;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,19 @@ namespace MissileCommand.Gameplay.Entities
     public class BasicDefenceBrain : Brain
     {
         [SerializeField] private Vector2 _target;
+        [SerializeField] private GameObjectVariable _reticle;
 
         public override void FindTarget()
         {
-            throw new System.NotImplementedException();
+            Vector3 reticlePosition = _reticle.RunTimeValue.gameObject.transform.position;
+            _target = new Vector2(reticlePosition.x, reticlePosition.y);
         }
 
         public override void Think(AILogicController thinker)
         {
+            if (_target == null)
+                FindTarget();
+
             thinker.MoveTowardsTarget(_target);
         }
     }
