484122b4a50e336b9cef0095cf109c5b4f351a48
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-18 23:54:12 +1000

    MainMenuButtonHover now works

--- Assets/Game/Scripts/UI/MainMenu/MainMenuButtonHover.cs
+++ Assets/Game/Scripts/UI/MainMenu/MainMenuButtonHover.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-namespace MissileCommand.UI
+namespace MissileCommand.UI.MainMenu
 {
-    public class MainMenuButtonHover : MonoBehaviour
+    public class MainMenuButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
-        // Start is called before the first frame update
-        void Start()
+        [Header("Components")]
+        [SerializeField] private Button _button;
+        [SerializeField] private TMP_Text _tmpText;
+
+        [Header("Settings")]
+        [SerializeField] private float _size = 32f;
+        [SerializeField] private float _hoverSize = 48f;
+        [SerializeField] private string _text;
+        [SerializeField] private string _hoverText;
+
+        public void OnPointerEnter(PointerEventData eventData)
         {
-        
+            _tmpText.text = _hoverText;
+            _tmpText.fontSize = _hoverSize;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void OnPointerExit(PointerEventData eventData)
         {
-        
+            _tmpText.text = _text;
+            _tmpText.fontSize = _size;
         }
     }
 }
