9c13a356933fbea9bd70c96553020ac353b54cf6
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-19 23:29:25 +1000

    Updated CameraScaler to adjust camera height

--- Assets/Game/Scripts/Gameplay/CameraScale.cs
+++ Assets/Game/Scripts/Gameplay/CameraScale.cs
@@ -6,11 +6,17 @@ namespace MissileCommand.Gameplay
     public class CameraScale : MonoBehaviour
     {
         [SerializeField] private bool _maintainWidth = true;
+        [SerializeField] private float _heightOffset = 0f;
 
         private float _defaultWidth;
+        private float _defaultHeight;
+        private Vector3 _cameraPosition;
 
         private void Start()
         {
+            _cameraPosition = Camera.main.transform.position;
+
+            _defaultHeight = Camera.main.orthographicSize;
             _defaultWidth = Camera.main.orthographicSize * Camera.main.aspect;
         }
 
@@ -19,6 +25,8 @@ namespace MissileCommand.Gameplay
             if (_maintainWidth)
             {
                 Camera.main.orthographicSize = _defaultWidth / Camera.main.aspect;
+
+                Camera.main.transform.position = new Vector3(_cameraPosition.x, -1 * (_defaultHeight - Camera.main.orthographicSize) + _heightOffset, _cameraPosition.z);
             }
         }
     }
