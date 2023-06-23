f116701539bb1f2e0fbd770c4a805b6eb69834fa
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-06-23 23:40:39 +1000

    Created ScoreData.cs

--- Assets/Game/Scripts/UI/GameOver/Highscores/HighscoreDisplay.cs
+++ Assets/Game/Scripts/UI/GameOver/Highscores/HighscoreDisplay.cs
@@ -1,3 +1,4 @@
+using MissileCommand.Gameplay;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,34 +6,18 @@ using UnityEngine;
 
 namespace MissileCommand.UI.GameOver
 {
-    public struct Highscore
-    {
-        public Highscore(string user, int score)
-        {
-            User = user;
-            Score = score;
-        }
-
-        public string User;
-        public int Score;
-    }
-
     public class HighscoreDisplay : MonoBehaviour
     {
         [SerializeField] private List<HighscoreItem> _highscoreItems = new List<HighscoreItem>();
-        private List<Highscore> _highscores = new List<Highscore>();
+
+        [SerializeField] private ScoreData _scoreData;
 
         private void Start()
         {
-            _highscores.Add(new Highscore("Scott", 1000));
-            _highscores.Add(new Highscore("Scott", 2000));
-            _highscores.Add(new Highscore("Scott", 3000));
-            _highscores.Add(new Highscore("Scott", 4000));
-            _highscores.Add(new Highscore("Scott", 5000));
 
             for (int i = 0; i < _highscoreItems.Count; i++)
             {
-                _highscoreItems[i].SetContent(_highscores[i].User, _highscores[i].Score);
+                _highscoreItems[i].SetContent(_scoreData._highscores[i].User, _scoreData._highscores[i].Value);
             }    
         }
     }
