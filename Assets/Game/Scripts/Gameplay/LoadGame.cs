942d7c7284567f9b49d7ae1fb97b8f84436f27f2
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-07-15 23:47:31 +1000

    Update LoadGame.cs

--- Assets/Game/Scripts/Gameplay/LoadGame.cs
+++ Assets/Game/Scripts/Gameplay/LoadGame.cs
@@ -6,7 +6,8 @@ namespace MissileCommand.Gameplay
     public class LoadGame : MonoBehaviour
     {
         [SerializeField] private SceneData _sceneData;
-        [SerializeField] private  ScoreData _scoreData;
+        [SerializeField] private ScoreData _scoreData;
+        [SerializeField] private string _saveDir;
 
         // Start is called before the first frame update
         void Start()
