b206e03beeebf60f4e013ad4b340ba49ff517a4e
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-07-16 22:40:38 +1000

    Update SaveDataManager.cs

--- Assets/Game/Scripts/Infrastructure/SaveDataManager.cs
+++ Assets/Game/Scripts/Infrastructure/SaveDataManager.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
-public static class SaveDataManager : MonoBehaviour
+public static class SaveDataManager
 {
-    
+    public static readonly string fileName = "/SaveData.save";
+
+    public static void SaveGameData(SaveData saveData)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        FileStream file;
+        file = File.Create(Application.persistentDataPath + fileName);
+        binaryFormatter.Serialize(file, saveData);
+        file.Close();
+    }
 }
