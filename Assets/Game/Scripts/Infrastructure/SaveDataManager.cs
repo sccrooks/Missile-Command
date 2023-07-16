13bcd64a8bc902e28a04a765f2f452df463d6f14
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-07-16 22:45:20 +1000

    Update SaveDataManager to include LoadGameData function

--- Assets/Game/Scripts/Infrastructure/SaveDataManager.cs
+++ Assets/Game/Scripts/Infrastructure/SaveDataManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using Unity.IO.LowLevel.Unsafe;
+using UnityEditor;
 using UnityEngine;
 
 public static class SaveDataManager
@@ -16,5 +17,25 @@ public static class SaveDataManager
         file = File.Create(Application.persistentDataPath + fileName);
         binaryFormatter.Serialize(file, saveData);
         file.Close();
+    }
+
+    public static SaveData LoadGameData()
+    {
+        if (File.Exists(Application.persistentDataPath + fileName))
+        {
+            try
+            {
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                FileStream file = File.Open(Application.persistentDataPath + fileName, FileMode.Open);
+                SaveData saveData = (SaveData)binaryFormatter.Deserialize(file);
+                file.Close();
+                return saveData;
+            } catch
+            {
+
+            }
+        }
+        else
+            return null;
     }
 }
