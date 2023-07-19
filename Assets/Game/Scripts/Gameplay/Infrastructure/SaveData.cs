7f8de900b3e6e40e2c248039ff3861ae85e3854b
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-07-19 23:35:52 +1000

    Update SaveData.cs

--- Assets/Game/Scripts/Gameplay/Infrastructure/SaveData.cs
+++ Assets/Game/Scripts/Gameplay/Infrastructure/SaveData.cs
@@ -8,6 +8,6 @@ namespace MissileCommand.Infrastructure
     [System.Serializable]
     public class SaveData
     {
-        public ScoreData ScoreData;
+        public List<Score> ScoreData = new List<Score>();
     }
 }
