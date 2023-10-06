cf803890d7dd146dbe4d269b2c6eec1c59fa010e
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-10-07 04:05:06 +1100

    ADDED console logs to save/load system

--- Assets/Game/Scripts/Gameplay/Infrastructure/SaveDataManager.cs
+++ Assets/Game/Scripts/Gameplay/Infrastructure/SaveDataManager.cs
@@ -36,6 +36,7 @@ namespace MissileCommand.Infrastructure
             }
             else
             {
+                Debug.LogWarning("Save data could not be found!");
                 SaveData tempData = new SaveData();
                 SaveGameData(tempData);
                 return tempData;
