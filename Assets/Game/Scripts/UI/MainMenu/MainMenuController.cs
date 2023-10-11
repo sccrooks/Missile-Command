8b664aeb69fcbf886a1d88287820d25f976933c0
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-10-11 23:51:59 +1100

    Updated menu controller to control sub-menus

--- Assets/Game/Scripts/UI/MainMenu/MainMenuController.cs
+++ Assets/Game/Scripts/UI/MainMenu/MainMenuController.cs
@@ -9,6 +9,15 @@ namespace MissileCommand.UI.MainMenu
         [SerializeField]
         private SceneData _sceneData;
 
+        [Header("Menus")]
+        [SerializeField] private GameObject _mainMenu;
+        [SerializeField] private GameObject _settingsMenu;
+
+        private void Start()
+        {
+            OpenMainMenu();
+        }
+
         /// <summary>
         /// Starts the game
         /// </summary>
@@ -27,10 +36,28 @@ namespace MissileCommand.UI.MainMenu
             SceneManager.LoadScene(_sceneData.Endless, LoadSceneMode.Single);
         }
 
-        [Command]
-        public void GameOver()
+        /// <summary>
+        /// Opens the main menu
+        /// </summary>
+        [Command("Open-Main-Menu")]
+        public void OpenMainMenu()
         {
-            throw new System.NotImplementedException();
+            _levelEditor.SetActive(false);
+            _settingsMenu.SetActive(false);
+
+            _mainMenu.SetActive(true);
+        }
+
+        /// <summary>
+        /// Opens the settings menu
+        /// </summary>
+        [Command("Open-Settings-Menu")]
+        public void OpenSettings()
+        {
+            _mainMenu.SetActive(false);
+            _levelEditor.SetActive(true);
+
+            _settingsMenu.SetActive(true);
         }
     }
 }
