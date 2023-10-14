2e1891259dd53f1c26e2d12bbe918d8fc3665d26
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-10-14 21:57:31 +1100

    Displaying ammo to screen

---
+++ Assets/Game/Scripts/UI/HUD/AmmoUpdater.cs
@@ -0,0 +1,42 @@
+using MissileCommand.Gameplay;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MissileCommand.UI.HUD
+{
+    public class AmmoUpdater : MonoBehaviour
+    {
+        [SerializeField] private Ammo _ammo;
+        [SerializeField] private GameObject _ammoPrefab;
+
+        private List<GameObject> _ammoGameobjects = new List<GameObject>();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            updateDisplay();
+        }
+
+        /// <summary>
+        /// Updates ammo count on HUD
+        /// </summary>
+        public void updateDisplay()
+        {
+            int ammo = _ammo.RuntimeValue;
+
+            while (_ammoGameobjects.Count < ammo)
+            {
+                GameObject ammoGameobject = Instantiate(_ammoPrefab, transform);
+                _ammoGameobjects.Add(ammoGameobject);
+            }
+
+            while (_ammoGameobjects.Count > ammo)
+            {
+                GameObject gameObject = _ammoGameobjects[_ammoGameobjects.Count - 1];
+                _ammoGameobjects.Remove(gameObject);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
